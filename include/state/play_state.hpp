bd93c9df6b09af7da6ce2cec19a7673bacf70a94
Hadenir <hadenir@gmail.com>
2020-04-23 15:36:15 +0200

    Update play_state.cpp/hpp

--- include/state/play_state.hpp
+++ include/state/play_state.hpp
@@ -1,15 +1,24 @@
 #pragma once
 
+#include "player.hpp"
 #include "game_state.hpp"
 #include "game_board.hpp"
+#include "network/connection.hpp"
 
 class PlayState : public GameState
 {
 private:
     const int GAMEBOARD_WIDTH = 100;
     const int GAMEBOARD_HEIGHT = 100;
+    const sf::IpAddress SERVER_ADDR = "127.0.0.1";
+    const unsigned short SERVER_PORT = 2704;
 
     GameBoard board;
+    std::vector<Player> players;
+
+    Connection server_connection;
+
+    void packet_received(std::unique_ptr<Packet> packet);
 
 public:
     PlayState(sf::RenderWindow& window);
--- src/state/play_state.cpp
+++ src/state/play_state.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
-#include "network/connection.hpp"
-#include "network/packet/debug_packet.hpp"
 #include "state/play_state.hpp"
+#include "network/packet/packets.hpp"
+#include "network/connection.hpp"
 
 PlayState::PlayState(sf::RenderWindow& window)
-        : GameState(window), board(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT)
+        : GameState(window),
+          board(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT),
+          server_connection(SERVER_ADDR, SERVER_PORT)
 {
-
+    JoinGamePacket packet("General Kenobi");
+    std::cout << server_connection.send(packet) << std::endl;
 }
 
 std::unique_ptr<GameState> PlayState::handle_input(sf::Event event)
@@ -20,10 +23,47 @@ std::unique_ptr<GameState> PlayState::handle_input(sf::Event event)
 
 std::unique_ptr<GameState> PlayState::update(float dt)
 {
+    if(server_connection.is_connected())
+    {
+        auto packet = server_connection.recv();
+        if(packet != nullptr)
+            packet_received(std::move(packet));
+    }
+
     return nullptr;
 }
 
 void PlayState::render(float dt)
 {
 
+}
+
+void PlayState::packet_received(std::unique_ptr<Packet> packet)
+{
+    std::cout << "Server sent a packet!" << std::endl;
+
+    switch(packet->get_id())
+    {
+        case DebugPacket::PACKET_ID:
+        {
+            auto debug_packet = dynamic_cast<DebugPacket&>(*packet);
+            std::cout << "[Debug:" << server_connection.get_addr() << "]: "
+                      << debug_packet.get_message() << std::endl;
+
+            break;
+        }
+        case PlayersListPacket::PACKET_ID:
+        {
+            auto players_list_packet = dynamic_cast<PlayersListPacket&>(*packet);
+            players = players_list_packet.get_players_list();
+
+            std::cout << "Received players list: " << std::endl;
+            for(auto& player : players)
+                std::cout << "\t - " << player.get_nickname() << std::endl;
+
+            break;
+        }
+        default:
+            break;
+    }
 }
