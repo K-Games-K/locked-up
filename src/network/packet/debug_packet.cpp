37669dd63819cecd21b4ba5c87451455cf17bf01
Hadenir <hadenir@gmail.com>
2020-04-23 15:35:36 +0200

    Update debug_packet.cpp

--- src/network/packet/debug_packet.cpp
+++ src/network/packet/debug_packet.cpp
@@ -1,11 +1,11 @@
 #include "network/packet/debug_packet.hpp"
 
 DebugPacket::DebugPacket()
-    : Packet(PACKET_ID)
+        : Packet(PACKET_ID)
 {}
 
 DebugPacket::DebugPacket(const std::string& debug_msg)
-    : Packet(PACKET_ID), debug_msg(debug_msg)
+        : Packet(PACKET_ID), debug_msg(debug_msg)
 {}
 
 std::string DebugPacket::get_message() const
