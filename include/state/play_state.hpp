376e47250bc61c83899f80dfd607c55e35050d03
Hadenir <hadenir@gmail.com>
2020-04-23 12:59:33 +0200

    Edit play_state.cpp/hpp

--- include/state/play_state.hpp
+++ include/state/play_state.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
-#include <SFML/Window/Event.hpp>
-
 #include "game_state.hpp"
+#include "game_board.hpp"
 
 class PlayState : public GameState
 {
+private:
+    const int GAMEBOARD_WIDTH = 100;
+    const int GAMEBOARD_HEIGHT = 100;
+
+    GameBoard board;
+
 public:
     PlayState(sf::RenderWindow& window);
 
--- src/state/play_state.cpp
+++ src/state/play_state.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
+#include "network/connection.hpp"
+#include "network/packet/debug_packet.hpp"
 #include "state/play_state.hpp"
 
 PlayState::PlayState(sf::RenderWindow& window)
-        : GameState(window)
+        : GameState(window), board(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT)
 {
 
 }
 
 std::unique_ptr<GameState> PlayState::handle_input(sf::Event event)
 {
+    if(event.type == sf::Event::Closed)
+        window.close();
+
     return nullptr;
 }
 
@@ -20,5 +25,5 @@ std::unique_ptr<GameState> PlayState::update(float dt)
 
 void PlayState::render(float dt)
 {
-    window.display();
+
 }
