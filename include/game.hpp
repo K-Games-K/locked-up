3cc8c0de08d290a900172f387b8cf7fabf81bac0
Hadenir <hadenir@gmail.com>
2020-04-25 17:50:37 +0200

    Enlarge window

--- include/game.hpp
+++ include/game.hpp
@@ -9,8 +9,8 @@ class Game
 {
 private:
     const std::string WINDOW_NAME = "Locked Out! [InDev]";
-    const unsigned int WINDOW_WIDTH = 768;
-    const unsigned int WINDOW_HEIGHT = 768;
+    const unsigned int WINDOW_WIDTH = 1300;
+    const unsigned int WINDOW_HEIGHT = 900;
 
     std::unique_ptr<GameState> current_state;
 
